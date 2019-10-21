aeca94080b9a3fc83e8dcabbb9ecb5c9e3a2b8a4
Marcel Soltermann <souti11@hotmail.com>
2019-10-21 16:19:08 +0200

    [CHG] added Unit test and Geb config

---
+++ src/integration-test/resources/GebConfig.groovy
@@ -0,0 +1,25 @@
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+
+waiting {
+    timeout = 2
+}
+
+environments {
+
+    // run via “./gradlew chromeTest”
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chrome {
+        driver = { new ChromeDriver() }
+    }
+
+    // run via “./gradlew chromeHeadlessTest”
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chromeHeadless {
+        driver = {
+            ChromeOptions o = new ChromeOptions()
+            o.addArguments('headless')
+            new ChromeDriver(o)
+        }
+    }
+}
--- src/test/groovy/mvc/InPlaceCalculatorControllerSpec.groovy
+++ src/test/groovy/mvc/InPlaceCalculatorControllerSpec.groovy
@@ -2,12 +2,22 @@ package mvc
 
 import grails.testing.web.controllers.ControllerUnitTest
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link ControllerUnitTest} for usage instructions.
  */
 class InPlaceCalculatorControllerSpec extends Specification implements ControllerUnitTest<InPlaceCalculatorController> {
 
+    void "average of en:5 and exam:6 should be 6"(){
+        when:
+        def model = new CalculatorModel(en: 5, exam: 6)
+        controller.calc(model)
+        then:
+        model.result == "6"
+    }
+
+    @Unroll
     void "average of #en and #exam should be #result"(en, exam, result) {
         when:
             def model = new CalculatorModel(en:en, exam:exam)
@@ -16,7 +26,7 @@ class InPlaceCalculatorControllerSpec extends Specification implements Controlle
             model.result == result
         where:
             en  | exam | result
-            0.0 | 0.0  | "Cannot calculate. Exam value was invalid."
+            0.0 | 0.0  | "0"
             1.0 | 2.0  | "2"
             2.0 | 1.0  | "2"
             1.0 | 1.9  | "1"
